6a86f8dd65d4b65f7be406758af0f0b4b4b106f3
Davide Morazzo <morazzo.davide@gmail.com>
2023-03-10 13:03:57 +0100

    added VehicleOdometry to mocap_node

--- gz_groundtruth/src/motion_capture/motion_capture/mocap_node.py
+++ gz_groundtruth/src/motion_capture/motion_capture/mocap_node.py
@@ -1,11 +1,13 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.clock import Clock
 
 from nav_msgs.msg import Path
 from geometry_msgs.msg import (
     PoseArray, 
     Pose,
     PoseStamped)
+from px4_msgs.msg import VehicleOdometry
 
 import subprocess
 from math import sqrt
@@ -18,16 +20,17 @@ class MocapNode (Node):
         # Publishers
         self.pose_pub = self.create_publisher(PoseStamped, "/x500/pose", 10)
         self.path_pub = self.create_publisher(Path, "/x500/path", 10)
+        self.mocap_odom_pub = self.create_publisher(VehicleOdometry, "/fmu/in/vehicle_mocap_odometry", 10)
         # Messages
         self.vehicle_path_msg = Path()
+        self.vehicle_path_msg.header.frame_id = 'map'
         self.vehicle_pose_msg = PoseStamped()
         self.gz_pose_msg = Pose()
+        self.mocap_odom_msg = None
+        # Timers
+        self.get_timestamp = lambda : int(Clock().now().nanoseconds / 1000)
 
-        self.launch_ros_gz_bridge(
-            "/world/default/pose/info",
-            "geometry_msgs/msg/PoseArray",
-            "gz.msgs.Pose_V"
-        )
+        self.launch_ros_gz_bridge("/world/default/pose/info","geometry_msgs/msg/PoseArray","gz.msgs.Pose_V")
 
     def listener_cb(self, msg: PoseArray):
         # Nell'array di oggetti Pose il secondo è quello del drone a cui siamo interessati. L'ho
@@ -37,30 +40,14 @@ class MocapNode (Node):
         self.gz_pose_msg : Pose = msg.poses[1]
 
         # Change ref fram to FLU and publish msg
-        self.vehicle_pose_msg = PoseStamped()
-        self.vehicle_pose_msg.header.frame_id='map'
-        # Rotation from ENU to FLU reference frame -> Rot_z(-90) done both to 
-        # position and to orientation. 
-
-        # Position
-        self.vehicle_pose_msg.pose.position.x =  self.gz_pose_msg.position.y
-        self.vehicle_pose_msg.pose.position.y = -self.gz_pose_msg.position.x
-        self.vehicle_pose_msg.pose.position.z =  self.gz_pose_msg.position.z
-        # Orientation
-        x_gz = self.gz_pose_msg.orientation.x
-        y_gz = self.gz_pose_msg.orientation.y
-        z_gz = self.gz_pose_msg.orientation.z
-        w_gz = self.gz_pose_msg.orientation.w
-        self.vehicle_pose_msg.pose.orientation.x = (- x_gz - y_gz) * sqrt(2)/2
-        self.vehicle_pose_msg.pose.orientation.y = (- y_gz + x_gz) * sqrt(2)/2
-        self.vehicle_pose_msg.pose.orientation.z = (- z_gz + w_gz) * sqrt(2)/2
-        self.vehicle_pose_msg.pose.orientation.w = (- w_gz - z_gz) * sqrt(2)/2
-
+        self.vehicle_pose_msg = self.gazebo_to_flu_tf(self.gz_pose_msg)
         self.pose_pub.publish(self.vehicle_pose_msg)
-
-        self.vehicle_path_msg.header.frame_id = 'map'
+        # Publish path msg
         self.vehicle_path_msg.poses.append(self.vehicle_pose_msg)
         self.path_pub.publish(self.vehicle_path_msg)
+        # Create mocap odometry msg and publish to PX4
+        self.mocap_odom_msg = self.create_odometry_msg(self.vehicle_pose_msg)
+        # self.mocap_odom_pub.publish(self.mocap_odom_msg)
 
     def launch_ros_gz_bridge(self, topic:str, ros_type:str, gz_type:str):
         """ Launch subprocess to bridge Gazebo and ROS2 """
@@ -68,6 +55,51 @@ class MocapNode (Node):
         argument = f"{topic}@{ros_type}[{gz_type}"
         command = "ros2 run ros_gz_bridge parameter_bridge "+argument
         subprocess.Popen(command.split(' '))
+
+
+    def gazebo_to_flu_tf(self, gz_pose: Pose) -> PoseStamped:
+        """
+        Transform from gazebo Pose to FLU pose for ROS2. Is done doing
+        a rotation of -90° arounf z axis (global frame). 
+        """
+        vehicle_pose_msg = PoseStamped()
+        # vehicle_pose_msg.header.stamp = self.get_timestamp()
+        vehicle_pose_msg.header.frame_id='map'
+        # Position
+        vehicle_pose_msg.pose.position.x =  gz_pose.position.y
+        vehicle_pose_msg.pose.position.y = -gz_pose.position.x
+        vehicle_pose_msg.pose.position.z =  gz_pose.position.z
+        # Orientation
+        x_gz = gz_pose.orientation.x
+        y_gz = gz_pose.orientation.y
+        z_gz = gz_pose.orientation.z
+        w_gz = gz_pose.orientation.w
+        vehicle_pose_msg.pose.orientation.x = (- x_gz - y_gz) * sqrt(2)/2
+        vehicle_pose_msg.pose.orientation.y = (- y_gz + x_gz) * sqrt(2)/2
+        vehicle_pose_msg.pose.orientation.z = (- z_gz + w_gz) * sqrt(2)/2
+        vehicle_pose_msg.pose.orientation.w = (- w_gz - z_gz) * sqrt(2)/2
+        return vehicle_pose_msg
+    
+
+    def flu_to_ned_tf(self, pose : Pose) -> Pose:
+        """
+        Transform a FLU frame into NED
+        """
+        # TODO: da scrivere
+        pass 
+
+    
+    def create_odometry_msg(self, pose : Pose) -> VehicleOdometry:
+        """
+        Create VehicleOdometry message. Not include velocities
+        """
+        odom_msg = VehicleOdometry()
+        odom_msg.timestamp = self.get_timestamp()
+        odom_msg.pose_frame = VehicleOdometry.POSE_FRAME_NED
+        ned_pose = self.flu_to_ned_tf(pose)
+        odom_msg.position = ned_pose.position
+        odom_msg.q = ned_pose.orientation
+        return odom_msg
 
 def main(args=None):
     rclpy.init(args=args)
